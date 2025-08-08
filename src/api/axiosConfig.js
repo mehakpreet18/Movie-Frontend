648ddd891df8f71748d6808025897e5ef79de962
mehakpreet18 <mehak18preet@gmail.com>
2025-08-09 00:56:40 +0530

    changed backend port

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'https://9c96-103-106-239-104.ap.ngrok.io',
-    headers: {"ngrok-skip-browser-warning": "true"}
+    baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080'
 });
